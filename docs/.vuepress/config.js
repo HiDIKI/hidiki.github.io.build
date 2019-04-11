27c40eec1e1ff150ee6072c25f2d117f06145216
hidekuma <d.hidekuma@gmail.com>
2019-04-12 00:48:00 +0900

    init HiDIKI

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  title: `HiDIKI :: Hidekuma\'s WIKI`,
+  description: `Hidekuma\'s DEV logs`,
+  base: "/HiDIKI/",
+  dest: 'docs',
+  head: [
+    ['link', {
+      rel: 'icon',
+      href: '/logo.png'
+    }]
+  ],
+  themeConfig: {
+    sidebar: [
+      {
+        title: '1. test',
+        children: ['test/one'] 
+      }
+    ],
+    nav: [{
+        text: 'GitHub',
+        link: 'https://github.com/hidekuma/'
+      }, {
+        text: 'Blog',
+        link: 'https://hidekuma.github.io/'
+      }
+    ]
+  },
+}
+
