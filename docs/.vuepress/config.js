0592f915c06e1ce2785dd861fbbbbc1e5c09423a
hidekuma <d.hidekuma@gmail.com>
2019-04-12 09:50:26 +0900

    updated configs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,29 +1,18 @@
 module.exports = {
-  title: `HiDIKI :: Hidekuma\'s WIKI`,
-  description: `Hidekuma\'s DEV logs`,
-  base: "/HiDIKI/",
-  dest: 'docs',
-  head: [
-    ['link', {
-      rel: 'icon',
-      href: '/logo.png'
-    }]
-  ],
-  themeConfig: {
-    sidebar: [
-      {
-        title: '1. test',
-        children: ['test/one'] 
-      }
+    title: `HiDIKI :: Hidekuma\'s WIKI`,
+    description: `Hidekuma\'s DEV logs`,
+    base: "/HiDIKI/",
+    dest: 'docs',
+    head: [
+        ['link', { rel: 'icon', href: '/logo.png' }]
     ],
-    nav: [{
-        text: 'GitHub',
-        link: 'https://github.com/hidekuma/'
-      }, {
-        text: 'Blog',
-        link: 'https://hidekuma.github.io/'
-      }
-    ]
-  },
+    themeConfig: {
+        sidebar: [
+            {title: '1. test', children: ['/test/one', '/test/two']}
+        ],
+        nav: [
+            { text: 'GitHub', link: 'https://github.com/hidekuma/' }, { text: 'Blog', link: 'https://hidekuma.github.io/' }
+        ]
+    },
 }
 
