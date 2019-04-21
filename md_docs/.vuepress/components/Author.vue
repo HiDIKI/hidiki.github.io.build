d8e66a2098c69fe6343f206b3812a42141d19d93
hidekuma <d.hidekuma@gmail.com>
2019-04-21 20:56:56 +0900

    Added components for author (vuepress customizing)

---
+++ md_docs/.vuepress/components/Author.vue
@@ -0,0 +1,11 @@
+<template>
+    <div v-if="$page.frontmatter.customauthor">
+        <div v-for="value in $page.frontmatter.customauthor">
+            <p style="text-align:right" class="author" v-if="value.name">
+				Written by📝{{value.name}}
+				<span v-if="value.email"> / {{value.email}}</span></p>
+        </div>
+		<!-- <img :src="$withBase('/authors/lombok-error.png')" alt="lombok-error"> -->
+    </div>
+</template>
+
