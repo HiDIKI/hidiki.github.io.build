364d0cde1a65c9911c4f20c93b8450ea5e40c15c
hidekuma <d.hidekuma@gmail.com>
2019-04-21 21:47:22 +0900

    Updated author.vue

--- md_docs/.vuepress/components/Author.vue
+++ md_docs/.vuepress/components/Author.vue
@@ -3,7 +3,7 @@
         <div v-for="value in $page.frontmatter.customauthor">
             <p style="text-align:right" class="author" v-if="value.name">
 				Written by📝{{value.name}}
-				<span v-if="value.email"> / {{value.email}}</span></p>
+				<!-- <span v-if="value.email"> / {{value.email}}</span> --></p>
         </div>
 		<!-- <img :src="$withBase('/authors/lombok-error.png')" alt="lombok-error"> -->
     </div>
